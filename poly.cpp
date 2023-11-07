75f0dbe6607f9cb57d83a99bcc7132e35a646b94
”Yana <libina.ym@gmail.com>
2023-11-08 01:51:48 +0300

    code review

--- poly.cpp
+++ poly.cpp
@@ -1,7 +1,23 @@
 #include "poly.h"
 #include <cmath>
 
-Poly::Poly(std::initializer_list<int64_t> coefs) {
+using namespace Project;
+
+int64_t Project::Poly::FastPow(int64_t x, int64_t deg) const {
+    int64_t res = 1;
+    while (deg != 0) {
+        if (deg % 2 == 0) {
+            deg /= 2;
+            x *= x;
+        } else {
+            deg--;
+            res *= x;
+        }
+    }
+    return res;
+}
+
+Project::Poly::Poly(std::initializer_list<int64_t> coefs) {
     size_t i = 0;
     for (auto it = coefs.begin(); it < coefs.end(); ++it) {
         if (*it != 0) {
@@ -11,7 +27,7 @@ Poly::Poly(std::initializer_list<int64_t> coefs) {
     }
 }
 
-Poly::Poly(std::initializer_list<std::pair<int64_t, int64_t>> deg_coef) {
+Project::Poly::Poly(std::initializer_list<std::pair<int64_t, int64_t>> deg_coef) {
     for (auto it = deg_coef.begin(); it < deg_coef.end(); ++it) {
         if (it->second != 0) {
             polynomial_[it->first] = it->second;
@@ -19,7 +35,7 @@ Poly::Poly(std::initializer_list<std::pair<int64_t, int64_t>> deg_coef) {
     }
 }
 
-void Poly::RemoveZeros(Poly& poly) const {
+void Project::Poly::RemoveZeros(Poly& poly) const {
     for (auto it = poly.polynomial_.begin(); it != poly.polynomial_.end();) {
         if (it->second == 0) {
             it = poly.polynomial_.erase(it);
@@ -29,15 +45,15 @@ void Poly::RemoveZeros(Poly& poly) const {
     }
 }
 
-int64_t Poly::operator()(int64_t x) const {
+int64_t Project::Poly::operator()(int64_t x) const {
     int64_t val = 0;
     for (auto mon : this->polynomial_) {
-        val += static_cast<int64_t>(std::pow(x, mon.first)) * mon.second;
+        val += FastPow(x, mon.first) * mon.second;
     }
     return val;
 }
 
-const bool Poly::operator==(const Poly& poly1) const {
+const bool Project::Poly::operator==(const Poly& poly1) const {
 
     if (this->polynomial_.size() != poly1.polynomial_.size()) {
         return false;
@@ -51,11 +67,11 @@ const bool Poly::operator==(const Poly& poly1) const {
     return true;
 }
 
-const bool Poly::operator!=(const Poly& poly1) const {
+const bool Project::Poly::operator!=(const Poly& poly1) const {
     return (!(*this == poly1));
 }
 
-Poly Poly::operator+(const Poly& poly1) const {
+Poly Project::Poly::operator+(const Poly& poly1) const {
     Poly poly = *this;
 
     for (auto mon : poly1.polynomial_) {
@@ -66,7 +82,7 @@ Poly Poly::operator+(const Poly& poly1) const {
     return poly;
 }
 
-Poly Poly::operator-() const {
+Poly Project::Poly::operator-() const {
     Poly poly = *this;
     for (auto mon : this->polynomial_) {
         poly.polynomial_[mon.first] = -mon.second;
@@ -74,20 +90,20 @@ Poly Poly::operator-() const {
     return poly;
 }
 
-Poly Poly::operator-(const Poly& poly1) const {
+Poly Project::Poly::operator-(const Poly& poly1) const {
     return *this + (-poly1);
 }
 
-Poly Poly::operator+=(const Poly& poly1) {
+Poly Project::Poly::operator+=(const Poly& poly1) {
     *this = *this + poly1;
     return *this;
 }
 
-Poly Poly::operator-=(const Poly& poly1) {
+Poly Project::Poly::operator-=(const Poly& poly1) {
     return *this += (-poly1);
 }
 
-Poly Poly::operator*(const Poly& poly1) const {
+Poly Project::Poly::operator*(const Poly& poly1) const {
     Poly poly;
 
     for (auto mon : this->polynomial_) {
@@ -100,7 +116,7 @@ Poly Poly::operator*(const Poly& poly1) const {
     return poly;
 }
 
-Poly Poly::operator*=(const Poly& poly1) {
+Poly Project::Poly::operator*=(const Poly& poly1) {
     *this = *this * poly1;
     return *this;
 }
--- poly.h
+++ poly.h
@@ -6,28 +6,43 @@
 #include <sstream>
 #include <tuple>
 
-class Poly {
-public:
-    Poly() = default;
-    Poly(std::initializer_list<int64_t> coefs);
-    Poly(std::initializer_list<std::pair<int64_t, int64_t>> deg_coef);
+namespace Project {
 
-    int64_t operator()(int64_t x) const;
-    const bool operator==(const Poly& poly1) const;
-    const bool operator!=(const Poly& poly1) const;
+    class Poly {
+    public:
+        Poly() = default;
 
-    Poly operator+(const Poly& poly1) const;
-    Poly operator-() const;
-    Poly operator-(const Poly& poly1) const;
-    Poly operator+=(const Poly& poly1);
-    Poly operator-=(const Poly& poly1);
-    Poly operator*(const Poly& poly1) const;
-    Poly operator*=(const Poly& poly1);
+        Poly(std::initializer_list <int64_t> coefs);
 
-    friend std::ostream& operator<<(std::ostream& stream, const Poly& poly);
+        Poly(std::initializer_list <std::pair<int64_t, int64_t>> deg_coef);
 
-private:
-    std::map<size_t, int64_t, std::greater<size_t>> polynomial_;
+        int64_t operator()(int64_t x) const;
 
-    void RemoveZeros(Poly& poly) const;
-};
+        const bool operator==(const Poly &poly1) const;
+
+        const bool operator!=(const Poly &poly1) const;
+
+        Poly operator+(const Poly &poly1) const;
+
+        Poly operator-() const;
+
+        Poly operator-(const Poly &poly1) const;
+
+        Poly operator+=(const Poly &poly1);
+
+        Poly operator-=(const Poly &poly1);
+
+        Poly operator*(const Poly &poly1) const;
+
+        Poly operator*=(const Poly &poly1);
+
+        friend std::ostream &operator<<(std::ostream &stream, const Poly &poly);
+
+    private:
+        std::map <size_t, int64_t, std::greater<size_t>> polynomial_;
+
+        void RemoveZeros(Poly &poly) const;
+
+        int64_t FastPow(int64_t x, int64_t deg) const;
+    };
+}
